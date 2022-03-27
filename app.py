846387a57a1629127947e5357870d6111ab17329
MichalPawel <pawelmm96@gmail.com>
2022-03-27 19:42:05 +0200

    add menu

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def show_tasks():
+    # task_index = 0
+    # cls()
+    # print("Current tasks: ")
+    # if len(tasks) < 1:
+    #     print("No taks")
+    # for task in tasks:
+    #     print(str(task_index + 1) + ". " + task)
+    #     task_index += 1
+    print("show tasks")
+
+
+def add_task():
+    # task = input("Type task name: ")
+    # tasks.append(task)
+    # cls()
+    # print('Task added: ' + task)
+    print("add task")
+
+
+def delete_task():
+    # id_to_be_deleted_plus_1 = input("Type number of task to be deleted: ")
+    # try:
+    #     cls()
+    #     print("Deleted task: " + tasks.pop(int(id_to_be_deleted_plus_1) - 1))
+    # except:
+    #     cls()
+    #     print('Oops Something went wrong. Make sure you put a correct task number')
+    print("delete task")
+
+
+def show_menu():
+    print()
+    print('1. Show tasks')
+    print('2. Add task')
+    print('3. Delete task')
+    print('4. Exit')
+    global user_choice
+    user_choice = int(input("Select number: "))
+
+
+while True:
+    show_menu()
+    if user_choice == 1:
+        show_tasks()
+    if user_choice == 2:
+        add_task()
+    if user_choice == 3:
+        delete_task()
+    if user_choice == 4:
+        break
